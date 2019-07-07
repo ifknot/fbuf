fb1a899cf3633aaf2b099c8ca6daa7d46bc588b1
Jeremy Thornton <headwedge@hotmail.co.uk>
2019-07-07 21:16:12 +0100

    fast pixel

--- frame_buffer.cpp
+++ frame_buffer.cpp
@@ -42,7 +42,8 @@ namespace linux_util {
     }
 
     void frame_buffer::pixel(uint32_t x, uint32_t y, pixel_t colour) {
-        uint32_t offset = (x + vinfo.xoffset) * (vinfo.bits_per_pixel / 8) + (y + vinfo.yoffset) * finfo.line_length;
+        //uint32_t offset = (x + vinfo.xoffset) * (vinfo.bits_per_pixel / 8) + (y + vinfo.yoffset) * finfo.line_length;
+        uint32_t offset = (x + vinfo.xoffset) * 2 + (y + vinfo.yoffset) * finfo.line_length;
         *((uint32_t *) (fbmap + offset)) = colour;
     }
 
