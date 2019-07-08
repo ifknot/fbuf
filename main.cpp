84988853a8d43da798ea56fb4096083a2384c86c
Jeremy Thornton <headwedge@hotmail.co.uk>
2019-07-08 21:35:37 +0100

    swap

--- main.cpp
+++ main.cpp
@@ -9,14 +9,14 @@ int main() {
 
     fbuf_t fb;
 
-
+/*
     for(uint32_t x = 0; x < 320; ++x) {
         fb.fill(fb.rgb(0xFF, 0xFF, 0xFF));
         fb.pixel(x, x, fb.rgb(0xFF,0,0));
         usleep(5000);
 
     }
-
+*/
     fb.clear();
 
     std::cout << fb.info() << std::endl;
