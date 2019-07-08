a63ad0f9a2199b26f6613b9cdf3d33f9af6ad920
Jeremy Thornton <headwedge@hotmail.co.uk>
2019-07-08 21:34:24 +0100

    swap

--- main.cpp
+++ main.cpp
@@ -8,22 +8,18 @@ int main() {
     using fbuf_t = linux_util::frame_buffer;
 
     fbuf_t fb;
-    //fb.swap();
 
-    //fb.open_buffer();
-/*
+
     for(uint32_t x = 0; x < 320; ++x) {
         fb.fill(fb.rgb(0xFF, 0xFF, 0xFF));
         fb.pixel(x, x, fb.rgb(0xFF,0,0));
         usleep(5000);
 
     }
-*/
-    //fb.clear();
-    //fb.swap();
-    std::cout << fb.info() << std::endl;
 
-    //fb.close_buffer();
+    fb.clear();
+
+    std::cout << fb.info() << std::endl;
 
     return 0;
 
