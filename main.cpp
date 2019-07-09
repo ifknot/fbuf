a41464e25ec206dd88304068c22b13c6ec5d508f
Jeremy Thornton <headwedge@hotmail.co.uk>
2019-07-09 20:53:06 +0100

    template

--- main.cpp
+++ main.cpp
@@ -9,15 +9,15 @@ int main() {
 
     //fbuf_t fb{"/dev/fb0"};
     fbuf_t fb;
-/*
+
     for(uint32_t x = 0; x < 320; ++x) {
         fb.pixel(x, x, fb.rgb(0xFF,0,0));
     }
-*/
+
     //fb.clear();
 
     fb.fill(fb.rgb(0xff,0,0));
-    usleep(5000);
+    usleep(500000);
     fb.clear();
 
     fb.pixel(200,200,fb.rgb(0xff,0,0));
